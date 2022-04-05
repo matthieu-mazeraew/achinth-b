5711b0cdfc3523fd07d29a1531a33fea56d12da2
matthieu-mazeraew <abcd94521@sina.com>
2022-04-05 12:28:05 +0800

    new and spot

---
+++ exp/src/App.js
@@ -0,0 +1,90 @@
+import React from "react";
+import "./App.css";
+import { FaLinkedin, FaEnvelope, FaGithub } from "react-icons/fa";
+import Dimension from "./components/Dimension";
+import Typewriter from "typewriter-effect";
+
+function App() {
+  let year = new Date();
+  const email = <FaEnvelope className="icon" />;
+  const linkedin = <FaLinkedin className="icon" />;
+  const github = <FaGithub className="icon" />;
+
+  const title = (
+    <div className="title">
+      <Typewriter
+        options={{
+          strings: ["achinth bharadwaj", "ಅಚಿಂತ್ ಭಾರದ್ವಾಜ್"],
+          autoStart: true,
+          loop: true,
+        }}
+      />
+    </div>
+  );
+  const subtitle = (
+    <div className="sub">
+      fourth year computer science, statistics and data science student @ ubc.
+    </div>
+  );
+
+  const workVibe = (
+    <div className="sub">
+      about to engineer @ tesla. <br />
+      former @ boeing and @ covalent <br />
+      always hunting for internships and fellowships in machine learning,
+      software dev and data science.
+    </div>
+  );
+
+  const links = (
+    <div>
+      <button
+        className="button"
+        onClick={() => window.open("https://linkedin.com/in/achinthb")}
+      >
+        {linkedin}
+      </button>
+
+      <button
+        className="button"
+        onClick={() => window.open("https://github.com/achinth-b")}
+      >
+        {github}
+      </button>
+
+      <button
+        className="button"
+        onClick={() =>
+          window.open("mailto:achinth@student.ubc.ca?subject=Hey Achinth!")
+        }
+      >
+        {email}
+      </button>
+    </div>
+  );
+
+  return (
+    <React.Fragment>
+      <div className="App">
+        <div className="App-header">
+          {title}
+
+          <Dimension />
+
+          {subtitle}
+          {workVibe}
+
+          <br />
+          {links}
+        </div>
+
+        <div className="footer">
+          {" "}
+          made with 💖, achinth © {year.getFullYear()}.{" "}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
